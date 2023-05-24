fc8fd639dbd16600585edd41fbc82cd4bb51c138
rima-nemri <rima.nemri@esprit.tn>
2023-05-24 15:20:28 +0200

    make static folder for all site

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,8 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     #add my apps
-    "blog.app.BlogConfig",
-    "user.app.UserConfig",
+    "blog.apps.BlogConfig",
+    "user.apps.UserConfig",
 
 ]
 
@@ -120,7 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+#if you want to make static folder ofr all you project use this method and put static folder under src
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+    r'C:\Users\user\Documents\djangoapps\Blogger\src\static',
 
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
