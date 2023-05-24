de47bc4ead92d2eb6de1ae46982e9269f6e174e7
rima-nemri <rima.nemri@esprit.tn>
2023-05-24 14:18:48 +0200

    make basic setting for my project

--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #add my apps
+    "blog.app.BlogConfig",
+    "user.app.UserConfig",
+
 ]
 
 MIDDLEWARE = [
