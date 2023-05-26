7e723c41d9d576bbe620014e2abf9ad0eafd7c04
rima-nemri <rima.nemri@esprit.tn>
2023-05-27 00:02:06 +0200

    create tab job

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+#on dirait on a creer un tableau nome profile et on y ajoute des colonnes 
+'''
+#django model field :
+    -html widget dans admin page de django
+    -validation(ex verifer si un email par exemple)
+    -db size of the field
+    python manage.py makemigrations #verifier si cest correcte ou pas
+    python manage.py migrate # do it
+'''
+
+class job (models.Model):#tab(class)
+    title=models.CharField(max_length=100) #100 #first column (field)
--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -122,11 +122,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 #if you want to make static folder ofr all you project use this method and put static folder under src
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static"),
-    r'C:\Users\user\Documents\djangoapps\Blogger\src\static',
-
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR,"static"),
+#    r'C:\Users\user\Documents\djangoapps\Blogger\src\static',
+#]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
